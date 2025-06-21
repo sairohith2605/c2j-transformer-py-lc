306db3fb50264937129f45da2e0503d8aaedb781
Sai Rohith Reddy <sairohith2605@outlook.com>
2025-06-22 00:15:51 +0530

    Add application menu header

--- processor-ux/src/app/app.component.ts
+++ processor-ux/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ClarityModule } from "@clr/angular";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ClarityModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ processor-ux/src/app/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ClarityModule } from '@clr/angular';
+
+@Component({
+  selector: 'app-header',
+  imports: [ClarityModule, RouterModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+  readonly appBrandRoute: RouteDefinition = {
+    label: 'CJ Transformer',
+    route: '/'
+  };
+
+}
+
+export interface RouteDefinition {
+
+  label: string;
+  route: string;
+}
