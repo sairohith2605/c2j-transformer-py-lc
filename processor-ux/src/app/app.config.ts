84faf25e71af1992e751adc80e176b68dcd08359
Sai Rohith Reddy <sairohith2605@outlook.com>
2025-06-22 20:16:09 +0530

    Install and configure monaco editor

--- processor-ux/src/app/app.config.ts
+++ processor-ux/src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+    importProvidersFrom(MonacoEditorModule.forRoot())
+  ]
 };
