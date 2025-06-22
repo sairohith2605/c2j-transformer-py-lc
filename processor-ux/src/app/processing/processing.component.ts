6c97a05675c20bd15d750acdce31ad6f9e2f67e7
Sai Rohith Reddy <sairohith2605@outlook.com>
2025-06-22 20:16:31 +0530

    Add code editor to accept json array content

--- processor-ux/src/app/processing/processing.component.ts
+++ processor-ux/src/app/processing/processing.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ClarityModule } from '@clr/angular';
+import { MonacoEditorModule } from 'ngx-monaco-editor-v2';
 
 @Component({
   selector: 'app-processing',
-  imports: [],
+  imports: [ClarityModule, MonacoEditorModule, FormsModule],
   templateUrl: './processing.component.html',
   styleUrl: './processing.component.scss'
 })
 export class ProcessingComponent {
+
+  code: string | null = "// Type in your JSON Array here";
+  editorOptions = { theme: 'vs-dark', language: 'json', automaticLayout: true, fontSize: 14, fontFamily: 'Cascadia Code' };
 
 }
