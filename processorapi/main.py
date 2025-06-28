2418ee18474cf2a7f124b95b5353dc7e0f9f6db7
Sai Rohith Reddy <sairohith2605@outlook.com>
2025-06-28 13:21:16 +0530

    Whitelist frontend url for cors

--- processorapi/main.py
+++ processorapi/main.py
@@ -1,11 +1,21 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException, Response, UploadFile
-from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 from processor import Processor
 
 app = FastAPI()
 processor = Processor()
+
+origins = ["http://localhost:4200"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 load_dotenv()
 
